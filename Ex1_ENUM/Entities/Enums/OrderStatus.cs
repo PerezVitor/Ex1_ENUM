a4a89d802233e88641d5b37678dc307a0d7561f5
Vitor Perez <vitor.g.perez@hotmail.com>
2018-12-15 13:09:45 -0200

    Adicionado subpasta Enums com uma classe OrderStatus

---
+++ Ex1_ENUM/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1_ENUM.Entities.Enums
+{
+    class OrderStatus
+    {
+    }
+}
